12117fc3adf620a4a40d90a26e4f1193afdd90c0
JonasBeans <boon.jonas.git@outlook.com>
2023-02-19 13:24:49 +0100

    Add cross origin config for frontend application

--- airport-manager/src/main/java/be/jonasboon/airportmanager/AirportManagerApplication.java
+++ airport-manager/src/main/java/be/jonasboon/airportmanager/AirportManagerApplication.java
@@ -2,6 +2,9 @@ package be.jonasboon.airportmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AirportManagerApplication {
@@ -10,4 +13,14 @@ public class AirportManagerApplication {
 		SpringApplication.run(AirportManagerApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200")
+						.allowedMethods("*");
+			}
+		};
+	}
 }
